b385534d9aec4824e876a9a68806d11ba634d1b2
Kai McGregor <kairrmcgregor@gmail.com>
2022-07-19 19:59:22 -0400

    Added timer

--- unmonitor-downloaded-episodes.py
+++ unmonitor-downloaded-episodes.py
@@ -1,21 +1,26 @@
 from pyarr import SonarrAPI
 from sys import argv
+from time import time
 
 def unmonitor_all_downloaded(host, api_key):
+    def __unmonitor(episode):
+        episode['monitored'] = False
+        sonarr.upd_episode(episode['id'], episode)
+        counter[series['title']] = counter.get(series['title'], 0) + 1
+
+    start = time()
     sonarr = SonarrAPI(host, api_key)
     series_list = sonarr.get_series()
     counter = {}
     try:
         for series in series_list:
             episodes = sonarr.get_episodes_by_series_id(series['id'])
-            for episode in episodes:
-                if episode['hasFile'] and episode['monitored']:
-                    episode['monitored'] = False
-                    sonarr.upd_episode(episode['id'], episode)
-                    counter[series['title']] = counter.get(series['title'], 0) + 1
+            episodes = [__unmonitor(episode) for episode in episodes if episode['hasFile'] and episode['monitored']]
+                    
     finally:
         print('Unmonitored:')
         [print(f'\t{title}: {count} episodes') for title, count in counter.items()]
+        print(f'Elapsed: {time()-start:.2f} seconds')
 
 def main():
     if len(argv) < 3:
