38353f050d19cfc845dea7809339235deb9cfaa8
Kai McGregor <kairrmcgregor@gmail.com>
2022-08-15 21:59:58 -0400

    Removed seperate ID from Sonarr update episode command

--- unmonitorDownloadedEpisodes.py
+++ unmonitorDownloadedEpisodes.py
@@ -4,7 +4,6 @@ from sys import argv
 def unmonitor_all_downloaded(host, api_key):
     def __unmonitor(episode):
         episode['monitored'] = False
-        print(f'EPISODE: {episode}')
         sonarr.upd_episode(episode)
         counter[series['title']] = counter.get(series['title'], 0) + 1
 
@@ -14,13 +13,12 @@ def unmonitor_all_downloaded(host, api_key):
     try:
         for series in series_list:
             episodes = sonarr.get_episodes_by_series_id(series['id'])
-            print(len(episodes))
             episodes = [__unmonitor(episode) for episode in episodes if episode['hasFile'] and episode['monitored']]
                     
     finally:
         print('\nUnmonitored:')
         [print(f'\t{title}: {count} episodes') for title, count in counter.items()]
-        
+
 
 def main():
     if len(argv) < 3:
